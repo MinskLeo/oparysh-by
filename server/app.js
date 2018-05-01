dd993f37e6c349e1712a2df50ed9687e0763b194
MinskLeo <2119930@gmail.com>
2018-05-01 17:40:05 +0300

    Добавлена БД к серверу, выборка из коллекции категорий

--- server/app.js
+++ server/app.js
@@ -1,23 +1,61 @@
+// Importing modules
 const express = require("express");
 const app = express();
+const mongoose = require('mongoose');
 
+// Setting up some of them
+mongoose.connect('mongodb://localhost/oparysh');
 app.set("view engine", "ejs");
 app.use(express.static("assets"));
 
+// Creating variables
+const Product = mongoose.model('Product', { 
+  name: String,
+  image: String,
+  description: String
+ });
+const Category = mongoose.model('Category', {
+  name: String
+});
+
+let categories = [];
+
+
+// SETUP ENDED
+
+app.get('*', (req, res, next) => {
+  console.log(categories);
+  next();
+});
+
 
 app.get('/', (req, res) => {
-  res.render("index");
+  // нужно добавить категории
+  res.render("index", { categories: categories });
 });
 
 app.get('/company', (req, res) => {
-  res.render("index");
+  res.render("index", { categories: categories });
 });
 
 app.get('/cooperation', (req, res) => {
-  res.render("index");
+  res.render("index", { categories: categories });
 });
 
 
 app.listen(8080,()=>{
+  Category.find( (err,result) => {
+    if(err){
+      console.log("Error! : "+err.message);
+    }else{
+      result.forEach( item => {
+        categories.push(item.name);
+      });
+    }
+
+
+  })
+  
+
   console.log("oparysh.by has been started on: 8080");
 });
