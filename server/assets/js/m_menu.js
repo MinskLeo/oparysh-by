e3b05f6216c0a9020a47dab29c41ea6f4547881b
Pavel Goroshko <mcredpower@gmail.com>
2018-06-14 00:00:22 +0300

    added sql lines

--- server/assets/js/m_menu.js
+++ server/assets/js/m_menu.js
@@ -2,12 +2,10 @@ $("document").ready(() => {
   const checkbox = $("#m_btn");
   console.log(checkbox);
   checkbox.change(function() {
-
        if (checkbox.prop( "checked" ) == true) {
         $('#toggle').show("slow");
       } else {
         $('#toggle').hide("slow");
       };
-
     });
 });
